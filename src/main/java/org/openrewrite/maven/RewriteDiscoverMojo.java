fdea0184afbf9adcd56fe38cd3f9aa763a0bc079
Greg Adams <greg@moderne.io>
2021-03-01 14:16:39 -0800

    updated for recipe descriptor changes

--- src/main/java/org/openrewrite/maven/RewriteDiscoverMojo.java
+++ src/main/java/org/openrewrite/maven/RewriteDiscoverMojo.java
@@ -4,8 +4,7 @@ import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.openrewrite.Recipe;
-import org.openrewrite.config.Environment;
-import org.openrewrite.config.RecipeDescriptor;
+import org.openrewrite.config.*;
 
 import java.util.Collection;
 
@@ -18,24 +17,75 @@ public class RewriteDiscoverMojo extends AbstractRewriteMojo {
         Environment env = environment();
 
         Collection<Recipe> recipesByName = env.listRecipes();
-        log.info("Found " + activeRecipes.size() + " active recipes and " + recipesByName.size() + " total recipes.\n");
+        log.info("Found " + activeRecipes.size() + " active recipes and " + recipesByName.size() + " total recipes.");
+        log.info("");
 
         log.info("Active Recipes:");
         for (String activeRecipe : activeRecipes) {
-            log.info("\t" + activeRecipe);
+            log.info("    " + activeRecipe);
         }
         log.info("");
         log.info("All Recipes:");
         for (Recipe recipe : recipesByName) {
-            log.info("\tname: " + recipe.getName());
+            log.info("    name: " + recipe.getName());
         }
-
-        log.info("\nRecipe Descriptors:");
+        log.info("");
+        log.info("Recipe Descriptors:");
         for (RecipeDescriptor recipeDescriptor : env.listRecipeDescriptors()) {
-            log.info("\tname: " + recipeDescriptor.getName());
-            for (String parameter : recipeDescriptor.getParameters()) {
-                log.info("\t\tparameter: " + parameter);
+            log.info("    Recipe name: " + recipeDescriptor.getName());
+            log.info("    Recipe display name: " + recipeDescriptor.getDisplayName());
+            log.info("    Recipe description: " + recipeDescriptor.getDescription());
+            if (!recipeDescriptor.getTags().isEmpty()) {
+                StringBuilder tags = new StringBuilder();
+                recipeDescriptor.getTags().forEach(t -> tags.append(t).append(", "));
+                tags.delete(tags.length() - 1, tags.length());
+                log.info("    Recipe tags: " + tags.toString());
             }
+            if (!recipeDescriptor.getOptions().isEmpty()) {
+                log.info("    Options:");
+                for (OptionDescriptor optionDescriptor : recipeDescriptor.getOptions()) {
+                    log.info("        Option name: " + optionDescriptor.getName());
+                    log.info("        Option type: " + optionDescriptor.getType());
+                    log.info("        Option display name: " + optionDescriptor.getDisplayName());
+                    log.info("        Option description: " + optionDescriptor.getDescription());
+                    log.info("");
+                }
+            }
+            if (!recipeDescriptor.getRecipeList().isEmpty()) {
+                log.info("    Recipe list:");
+                for (ConfiguredRecipeDescriptor r : recipeDescriptor.getRecipeList()) {
+                    printConfiguredRecipeDescriptor(r, "        ");
+                }
+            }
+            if (recipeDescriptor.getOptions().isEmpty() && recipeDescriptor.getRecipeList().isEmpty()) {
+                log.info("");
+            }
+        }
+    }
+
+    private void printConfiguredRecipeDescriptor(ConfiguredRecipeDescriptor recipeDescriptor, String indent) {
+        log.info(indent + "Recipe name: " + recipeDescriptor.getName());
+        log.info(indent + "Recipe display name: " + recipeDescriptor.getDisplayName());
+        if (!recipeDescriptor.getOptions().isEmpty()) {
+            log.info(indent + "Options:");
+            for (ConfiguredOptionDescriptor optionDescriptor : recipeDescriptor.getOptions()) {
+                log.info(indent + "    Option name: " + optionDescriptor.getName());
+                log.info(indent + "    Option type: " + optionDescriptor.getType());
+                if (optionDescriptor.getValue() != null) {
+                    log.info(indent + "    Option value: " + optionDescriptor.getValue());
+                }
+                log.info("");
+            }
+        }
+        if (!recipeDescriptor.getRecipeList().isEmpty()) {
+            log.info(indent + "Recipe list:");
+            for (ConfiguredRecipeDescriptor r : recipeDescriptor.getRecipeList()) {
+                printConfiguredRecipeDescriptor(r, indent + "\t");
+                log.info("");
+            }
+        }
+        if (recipeDescriptor.getOptions().isEmpty() && recipeDescriptor.getRecipeList().isEmpty()) {
+            log.info("");
         }
     }
 }
