a3892227c48378b6f6e94573b814db21ae18b967
Sam Snyder <sam@moderne.io>
2021-03-08 14:28:24 -0800

    Re-enable tests now that they can be made compatible with Rewrite 7.0.0

--- src/it/publish-skip-cyclone-dx-bom/verify.groovy
+++
@@ -1,6 +0,0 @@
-File astJar = new File(basedir, "target/publish-skip-cyclone-dx-bom-0.1.0-SNAPSHOT-ast.jar")
-assert astJar.isFile()
-
-File cycloneDxBom = new File(basedir, "target/publish-skip-cyclone-dx-bom-0.1.0-SNAPSHOT-cyclonedx.xml")
-assert !cycloneDxBom.isFile() && !cycloneDxBom.exists()
-
