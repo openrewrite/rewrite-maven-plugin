f3ded84763b6869ef4f5fd39b677edff1627d254
Aaron Gershman <aegershman@gmail.com>
2021-04-07 10:26:47 -0500

    remove rewrite:diff goal for rewrite:dryRun usage instead

--- src/it/multi-module-project/verify.groovy
+++ src/it/multi-module-project/verify.groovy
@@ -1,9 +1,5 @@
-File aPatch = new File(basedir, "a/target/site/rewrite/rewrite.patch")
-assert aPatch.isFile()
 File aCycloneDxBom = new File(basedir, "a/target/a-0.1.0-SNAPSHOT-cyclonedx.xml")
 assert aCycloneDxBom.isFile()
 
-File bPatch = new File(basedir, "b/target/site/rewrite/rewrite.patch")
-assert bPatch.isFile()
 File bCycloneDxBom = new File(basedir, "b/target/b-0.1.0-SNAPSHOT-cyclonedx.xml")
 assert bCycloneDxBom.isFile()
--- src/it/single-project/verify.groovy
+++ src/it/single-project/verify.groovy
@@ -1,5 +1,2 @@
-File patch = new File(basedir, "target/site/rewrite/rewrite.patch")
-assert patch.isFile()
-
 File cycloneDxBom = new File(basedir, "target/single-project-0.1.0-SNAPSHOT-cyclonedx.xml")
 assert cycloneDxBom.isFile()
