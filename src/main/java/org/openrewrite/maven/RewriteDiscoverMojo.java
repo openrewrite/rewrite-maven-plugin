9d33832df560c8d12a66d1be640300549b762fdb
Aaron Gershman <aegershman@gmail.com>
2021-04-24 16:29:26 -0500

    recipe configuration reference information for discover help commands

--- src/main/java/org/openrewrite/maven/RewriteDiscoverMojo.java
+++ src/main/java/org/openrewrite/maven/RewriteDiscoverMojo.java
@@ -3,157 +3,122 @@ package org.openrewrite.maven;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
-import org.openrewrite.Recipe;
 import org.openrewrite.config.Environment;
 import org.openrewrite.config.OptionDescriptor;
 import org.openrewrite.config.RecipeDescriptor;
 import org.openrewrite.internal.lang.Nullable;
+import org.openrewrite.style.NamedStyles;
 
 import java.util.Collection;
+import java.util.HashSet;
 
 /**
  * Display available recipes found on the classpath.<br>
- * {@code ./mvnw rewrite:discover -Drecipe=<recipe-name>} to display recipe configuration details. For example:<br>
- * {@code ./mvnw rewrite:discover -Drecipe=org.openrewrite.java.format.AutoFormat}
+ * {@code ./mvnw rewrite:discover -Ddetail=true -Drecipe=<recipe-name>} to display recipe configuration details. For example:<br>
+ * {@code ./mvnw rewrite:discover -Ddetail=true -Drecipe=org.openrewrite.java.format.AutoFormat}
  */
 @Mojo(name = "discover", threadSafe = true)
 public class RewriteDiscoverMojo extends AbstractRewriteMojo {
 
+    private static final String RECIPE_NOT_FOUND_EXCEPTION_MSG = "Could not find recipe '%s' among available recipes";
+
     /**
      * The name of a specific recipe to show details for. For example:<br>
-     * {@code ./mvnw rewrite:discover -Drecipe=org.openrewrite.java.format.AutoFormat}
+     * {@code ./mvnw rewrite:discover -Ddetail=true -Drecipe=org.openrewrite.java.format.AutoFormat}
      */
     @Nullable
     @Parameter(property = "recipe")
-    String recipeFilter;
+    String recipe;
 
     /**
-     * Whether to show verbose details of recipes and options.
+     * Whether to display recipe details such as displayName, description, and configuration options.
      */
-    @Parameter(property = "rewrite.discover.verbose", defaultValue = "false")
-    boolean verbose;
+    @Parameter(property = "detail", defaultValue = "false")
+    boolean detail;
 
     /**
-     * Whether to show recipe details for recipes which use other recipes.
+     * The maximum level of recursion to display recipe descriptors under recipeList.
      */
-    @Parameter(property = "rewrite.discover.recursive", defaultValue = "false")
-    boolean recursive;
+    @Parameter(property = "recursion", defaultValue = "0")
+    int recursion;
 
     @Override
     public void execute() throws MojoExecutionException {
         Environment env = environment();
-
-        if (recipeFilter != null) {
-            RecipeDescriptor recipeDescriptor = env.listRecipeDescriptors().stream().filter(r -> r.getName().equalsIgnoreCase(recipeFilter)).findAny().orElse(null);
-            if (recipeDescriptor == null) {
-                getLog().info("Recipe " + recipeFilter + " not found.");
-            } else {
-                logRecipeDescriptor(recipeDescriptor, verbose, recursive);
+        Collection<RecipeDescriptor> availableRecipeDescriptors = env.listRecipeDescriptors();
+        if (recipe != null) {
+            RecipeDescriptor rd = getRecipeDescriptor(recipe, availableRecipeDescriptors);
+            writeRecipeDescriptor(rd, detail, 0, 0);
+        } else {
+            Collection<RecipeDescriptor> activeRecipeDescriptors = new HashSet<>();
+            for (String activeRecipe : activeRecipes) {
+                RecipeDescriptor rd = getRecipeDescriptor(activeRecipe, availableRecipeDescriptors);
+                activeRecipeDescriptors.add(rd);
             }
-            return;
-        }
-        Collection<Recipe> recipesByName = env.listRecipes();
-        getLog().info("Found " + activeRecipes.size() + " active recipes and " + recipesByName.size() + " activatable recipes.");
-        getLog().info("");
-
-        getLog().info("Active Recipes:");
-        for (String activeRecipe : activeRecipes) {
-            getLog().info(indent(1, activeRecipe));
-        }
-        getLog().info("");
-        getLog().info("Activatable Recipes:");
-        for (Recipe recipe : recipesByName) {
-            getLog().info(indent(1, recipe.getName()));
-        }
-        getLog().info("");
-        if (verbose) {
-            getLog().info("Descriptors:");
-            for (RecipeDescriptor recipeDescriptor : env.listRecipeDescriptors()) {
-                logRecipeDescriptor(recipeDescriptor, verbose, recursive);
-            }
+            writeDiscovery(availableRecipeDescriptors, activeRecipeDescriptors, env.listStyles());
         }
     }
 
-    private void logRecipeDescriptor(RecipeDescriptor recipeDescriptor, boolean verbose, boolean recursive) {
-        if (verbose) {
-            getLog().info(indent(1, "Name: " + recipeDescriptor.getName()));
-            getLog().info(indent(1, "Display name: " + recipeDescriptor.getDisplayName()));
-            getLog().info(indent(1, "Description: " + recipeDescriptor.getDescription()));
-            if (!recipeDescriptor.getTags().isEmpty()) {
-                getLog().info(indent(1, "Tags: " + String.join(",", recipeDescriptor.getTags())));
-            }
-        } else {
-            getLog().info(indent(1, recipeDescriptor.getName()));
+    private void writeDiscovery(Collection<RecipeDescriptor> availableRecipeDescriptors, Collection<RecipeDescriptor> activeRecipeDescriptors, Collection<NamedStyles> availableStyles) {
+        getLog().info(indent(0, "Available Recipes:"));
+        for (RecipeDescriptor recipeDescriptor : availableRecipeDescriptors) {
+            writeRecipeDescriptor(recipeDescriptor, detail, 0, 1);
         }
-        if (!recipeDescriptor.getOptions().isEmpty()) {
-            if (verbose) {
-                getLog().info(indent(1, "Options:"));
-            }
-            for (OptionDescriptor optionDescriptor : recipeDescriptor.getOptions()) {
-                StringBuilder optionBuilder = new StringBuilder(optionDescriptor.getName())
-                        .append(": ").append(optionDescriptor.getType());
-                if (optionDescriptor.isRequired()) {
-                    optionBuilder.append("!");
-                }
-                getLog().info(indent(2, optionBuilder));
-                if (verbose) {
-                    getLog().info(indent(2, "Display name: " + optionDescriptor.getDisplayName()));
-                    getLog().info(indent(2, "Description: " + optionDescriptor.getDescription()));
-                    getLog().info("");
-                }
-            }
+
+        getLog().info(indent(0, "Available Styles:"));
+        for (NamedStyles style : availableStyles) {
+            getLog().info(indent(1, "name: " + style.getName()));
         }
-        if (!recipeDescriptor.getRecipeList().isEmpty()) {
-            if (verbose) {
-                getLog().info(indent(1, "Recipe list:"));
-            }
-            for (RecipeDescriptor r : recipeDescriptor.getRecipeList()) {
-                logNestedRecipeDescriptor(r, verbose, recursive, 2);
-            }
-            if (verbose) {
-                getLog().info("");
-            }
+
+        getLog().info(indent(0, "Active Styles:"));
+        for (String activeStyle : activeStyles) {
+            getLog().info(indent(1, activeStyle));
         }
-        if (!verbose || (recipeDescriptor.getOptions().isEmpty() && recipeDescriptor.getRecipeList().isEmpty())) {
-            getLog().info("");
+
+        getLog().info(indent(0, "Active Recipes:"));
+        for (RecipeDescriptor recipeDescriptor : activeRecipeDescriptors) {
+            writeRecipeDescriptor(recipeDescriptor, detail, 0, 1);
         }
+
+        getLog().info(indent(0, ""));
+        getLog().info(indent(0, "Found " + availableRecipeDescriptors.size() + " available recipes and " + availableStyles.size() + " available styles."));
+        getLog().info(indent(0, "Configured with " + activeRecipeDescriptors.size() + " active recipes and " + activeStyles.size() + " active styles."));
     }
 
-    private void logNestedRecipeDescriptor(RecipeDescriptor recipeDescriptor, boolean verbose, boolean recursive, int indent) {
-        if (verbose) {
-            getLog().info(indent(indent, "Name: " + recipeDescriptor.getName()));
-            getLog().info(indent(indent, "Display name: " + recipeDescriptor.getDisplayName()));
-            getLog().info(indent(indent, "Description: " + recipeDescriptor.getDescription()));
-            if (!recipeDescriptor.getTags().isEmpty()) {
-                getLog().info(indent(indent, "Tags: " + String.join(",", recipeDescriptor.getTags())));
-            }
-        } else {
-            getLog().info(indent(indent, recipeDescriptor.getName()));
-        }
-        if (!recipeDescriptor.getOptions().isEmpty()) {
+    private void writeRecipeDescriptor(RecipeDescriptor rd, boolean verbose, int currentRecursionLevel, int indentLevel) {
+        if (currentRecursionLevel <= recursion) {
+            getLog().info(indent(indentLevel, "name: " + rd.getName()));
             if (verbose) {
-                getLog().info(indent(indent, "Options:"));
+                getLog().info(indent(indentLevel, "displayName: " + rd.getDisplayName()));
+                getLog().info(indent(indentLevel, "description: " + rd.getDescription()));
+
+                getLog().info(indent(indentLevel, "options: " + (rd.getOptions().isEmpty() ? "[]" : "")));
+                for (OptionDescriptor od : rd.getOptions()) {
+                    getLog().info(indent(indentLevel + 1, od.getName() + ": " + od.getType() + (od.isRequired() ? "!" : "")));
+                    getLog().info(indent(indentLevel + 2, "displayName: " + od.getDisplayName()));
+                    getLog().info(indent(indentLevel + 2, "description: " + od.getDescription()));
+                }
             }
-            for (OptionDescriptor optionDescriptor : recipeDescriptor.getOptions()) {
-                getLog().info(indent(indent + 1, optionDescriptor.getName() + ": " + optionDescriptor.getValue()));
+
+
+            if (!rd.getRecipeList().isEmpty() && (currentRecursionLevel + 1 <= recursion)) {
+                getLog().info(indent(indentLevel, "recipeList:"));
+                for (RecipeDescriptor r : rd.getRecipeList()) {
+                    writeRecipeDescriptor(r, verbose, currentRecursionLevel + 1, indentLevel + 1);
+                }
             }
+
             if (verbose) {
-                getLog().info("");
-            }
-        }
-        if (recursive && !recipeDescriptor.getRecipeList().isEmpty()) {
-            if (verbose) {
-                getLog().info(indent(indent, "Recipe list:"));
-            }
-            for (RecipeDescriptor nestedRecipeDescriptor : recipeDescriptor.getRecipeList()) {
-                logNestedRecipeDescriptor(nestedRecipeDescriptor, verbose, true, indent + 1);
-            }
-            if (verbose) {
-                getLog().info("");
+                getLog().info(indent(indentLevel, ""));
             }
         }
-        if (!verbose || (recipeDescriptor.getOptions().isEmpty() && recipeDescriptor.getRecipeList().isEmpty())) {
-            getLog().info("");
-        }
+
+    }
+
+    private static RecipeDescriptor getRecipeDescriptor(String recipe, Collection<RecipeDescriptor> recipeDescriptors) throws MojoExecutionException {
+        return recipeDescriptors.stream()
+                .filter(r -> r.getName().equalsIgnoreCase(recipe))
+                .findAny()
+                .orElseThrow(() -> new MojoExecutionException(String.format(RECIPE_NOT_FOUND_EXCEPTION_MSG, recipe)));
     }
 }
