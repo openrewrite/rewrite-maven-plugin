188eb901591ab8b1e881e124616abb057508484e
Aaron Gershman <5619476+aegershman@users.noreply.github.com>
2021-04-26 14:58:38 -0500

    recipe configuration reference information for discover help commands (#150)

--- src/test/java/org/openrewrite/maven/RewriteDiscoverIT.java
+++ src/test/java/org/openrewrite/maven/RewriteDiscoverIT.java
@@ -5,6 +5,7 @@ import com.soebes.itf.jupiter.extension.MavenJupiterExtension;
 import com.soebes.itf.jupiter.extension.MavenTest;
 import com.soebes.itf.jupiter.extension.SystemProperty;
 import com.soebes.itf.jupiter.maven.MavenExecutionResult;
+import org.junit.jupiter.api.Nested;
 
 import static com.soebes.itf.extension.assertj.MavenITAssertions.assertThat;
 
@@ -12,8 +13,59 @@ import static com.soebes.itf.extension.assertj.MavenITAssertions.assertThat;
 @MavenGoal("${project.groupId}:${project.artifactId}:${project.version}:discover")
 public class RewriteDiscoverIT {
 
+    @Nested
+    @MavenGoal("${project.groupId}:${project.artifactId}:${project.version}:discover")
+    class RecipeLookup {
+
+        @MavenTest
+        @SystemProperty(value = "detail", content = "true")
+        void rewrite_discover_detail(MavenExecutionResult result) {
+            assertThat(result)
+                    .isSuccessful()
+                    .out()
+                    .info()
+                    .matches(logLines ->
+                            logLines.stream().anyMatch(logLine -> logLine.contains("displayName"))
+                    )
+                    .matches(logLines ->
+                            logLines.stream().anyMatch(logLine -> logLine.contains("description"))
+                    )
+                    .matches(logLines ->
+                            logLines.stream().anyMatch(logLine -> logLine.contains("options"))
+                    );
+
+            assertThat(result).out().warn().isEmpty();
+        }
+
+        @MavenTest
+        @SystemProperty(value = "recipe", content = "org.openrewrite.JAVA.format.AutoFormAT")
+        void rewrite_discover_recipe_lookup_case_insensitive(MavenExecutionResult result) {
+            assertThat(result)
+                    .isSuccessful()
+                    .out()
+                    .info()
+                    .matches(logLines ->
+                            logLines.stream().anyMatch(logLine -> logLine.contains("org.openrewrite.java.format.AutoFormat"))
+                    );
+        }
+
+        @MavenTest
+        @SystemProperty(value = "recursion", content = "1")
+        void rewrite_discover_recursion(MavenExecutionResult result) {
+            assertThat(result)
+                    .isSuccessful()
+                    .out()
+                    .info()
+                    .matches(logLines ->
+                            logLines.stream().anyMatch(logLine -> logLine.contains("recipeList"))
+                    );
+
+            assertThat(result).out().warn().isEmpty();
+        }
+    }
+
     @MavenTest
-    void rewrite_discover_default_output(MavenExecutionResult result) {
+    void rewrite_discover_default(MavenExecutionResult result) {
         assertThat(result)
                 .isSuccessful()
                 .out()
@@ -22,45 +74,20 @@ public class RewriteDiscoverIT {
                         logLines.stream().anyMatch(logLine -> logLine.contains("org.openrewrite.java.format.AutoFormat"))
                 )
                 .matches(logLines ->
-                        logLines.stream().noneMatch(logLine -> logLine.contains("Descriptors"))
+                        logLines.stream().anyMatch(logLine -> logLine.contains("org.openrewrite.java.SpringFormat"))
                 );
 
         assertThat(result).out().warn().isEmpty();
     }
 
     @MavenTest
-    void rewrite_discover_reads_rewrite_yml(MavenExecutionResult result) {
+    void rewrite_discover_rewrite_yml(MavenExecutionResult result) {
         assertThat(result)
                 .isSuccessful()
                 .out()
                 .info()
                 .matches(logLines ->
                         logLines.stream().anyMatch(logLine -> logLine.contains("com.example.RewriteDiscoverIT.CodeCleanup"))
-                );
-
-        assertThat(result).out().warn().isEmpty();
-    }
-
-    @MavenTest
-    @SystemProperty(value = "recipe", content = "org.openrewrite.JAVA.format.AutoFormAT")
-    void rewrite_discover_recipe_lookup_case_insensitive(MavenExecutionResult result) {
-        assertThat(result)
-                .isSuccessful()
-                .out()
-                .info()
-                .matches(logLines ->
-                        logLines.stream().anyMatch(logLine -> logLine.contains("org.openrewrite.java.format.AutoFormat"))
-                );
-    }
-
-    @MavenTest
-    void rewrite_discover_verbose_output(MavenExecutionResult result) {
-        assertThat(result)
-                .isSuccessful()
-                .out()
-                .info()
-                .matches(logLines ->
-                        logLines.stream().anyMatch(logLine -> logLine.contains("Descriptors"))
                 );
 
         assertThat(result).out().warn().isEmpty();
