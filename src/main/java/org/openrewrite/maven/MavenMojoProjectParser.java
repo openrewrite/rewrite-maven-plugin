af19c2b92ce3fb007487c8f446941438079dbb52
Tyler Van Gorder <tkvangorder@users.noreply.github.com>
2021-11-03 12:04:29 -0700

    Just for Jonathan.

--- src/main/java/org/openrewrite/maven/MavenMojoProjectParser.java
+++ src/main/java/org/openrewrite/maven/MavenMojoProjectParser.java
@@ -195,12 +195,12 @@ public class MavenMojoProjectParser {
             sourceFiles.add(maven);
             alreadyParsed.add(maven.getSourcePath());
         }
-        //JavaParser will add SourceSet Markers to any Java SourceFile, so only adding the project provenance info to
-        //java source.
+        // JavaParser will add SourceSet Markers to any Java SourceFile, so only adding the project provenance info to
+        // java source.
         sourceFiles.addAll(ListUtils.map(javaParser.parse(mainJavaSources, baseDir, ctx),
                 addProvenance(baseDir, projectProvenance, generatedSourcePaths)));
         ResourceParser rp = new ResourceParser(logger, exclusions);
-        //Any resources parsed from "main/resources" should also have the main source set added to them.
+        // Any resources parsed from "main/resources" should also have the main source set added to them.
         sourceFiles.addAll(ListUtils.map(
                 rp.parse(baseDir, mavenProject.getBasedir().toPath().resolve("src/main/resources"), alreadyParsed),
                 addProvenance(baseDir, mainProvenance, null)));
@@ -216,12 +216,12 @@ public class MavenMojoProjectParser {
         List<Marker> testProvenance = new ArrayList<>(projectProvenance);
         testProvenance.add(javaParser.getSourceSet(ctx));
 
-        //JavaParser will add SourceSet Markers to any Java SourceFile, so only adding the project provenance info to
-        //java source.
+        // JavaParser will add SourceSet Markers to any Java SourceFile, so only adding the project provenance info to
+        // java source.
         sourceFiles.addAll(ListUtils.map(
                 javaParser.parse(listJavaSources(mavenProject.getBuild().getTestSourceDirectory()), baseDir, ctx),
                 addProvenance(baseDir, projectProvenance, null) ));
-        //Any resources parsed from "test/resources" should also have the test source set added to them.
+        // Any resources parsed from "test/resources" should also have the test source set added to them.
         sourceFiles.addAll(ListUtils.map(
                 rp.parse(baseDir, mavenProject.getBasedir().toPath().resolve("src/test/resources"), alreadyParsed),
                 addProvenance(baseDir, testProvenance, null)));
