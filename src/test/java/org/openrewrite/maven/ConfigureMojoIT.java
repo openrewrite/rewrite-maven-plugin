348b482ea30ae9fee95650e3b4a4c89a3c9d1dd0
Tim te Beek <tim.te.beek@jdriven.com>
2021-11-21 10:31:36 +0100

    Add ConfigureMojoIT

---
+++ src/test/java/org/openrewrite/maven/ConfigureMojoIT.java
@@ -0,0 +1,31 @@
+package org.openrewrite.maven;
+
+import com.soebes.itf.jupiter.extension.MavenGoal;
+import com.soebes.itf.jupiter.extension.MavenJupiterExtension;
+import com.soebes.itf.jupiter.extension.MavenTest;
+import com.soebes.itf.jupiter.extension.SystemProperties;
+import com.soebes.itf.jupiter.extension.SystemProperty;
+import com.soebes.itf.jupiter.maven.MavenExecutionResult;
+
+import static com.soebes.itf.extension.assertj.MavenITAssertions.assertThat;
+
+@MavenJupiterExtension
+@MavenGoal("${project.groupId}:${project.artifactId}:${project.version}:configure")
+@SystemProperties({
+        @SystemProperty(value = "activeRecipes", content = "org.openrewrite.java.testing.junit5.ParameterizedRunnerToParameterized"),
+        @SystemProperty(value = "dependencies", content = "org.openrewrite.recipe:rewrite-spring:4.14.1")
+})
+public class ConfigureMojoIT {
+
+    @MavenTest
+    void single_project(MavenExecutionResult result) {
+        assertThat(result)
+                .isSuccessful()
+                .out()
+                .info()
+                .matches(logLines -> logLines.stream()
+                        .anyMatch(logLine -> logLine.contains("Added rewrite-maven-plugin to")),
+                        "Logs success message");
+    }
+
+}
