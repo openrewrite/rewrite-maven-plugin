32cb9e84969491d6f3af0a734d240f02e9972985
Aaron Gershman <aegershman@gmail.com>
2021-12-08 02:21:39 -0600

    [#262] Test for uninterpolated settings repo urls

--- src/test/java/org/openrewrite/maven/MavenMojoProjectParserIT.java
+++ src/test/java/org/openrewrite/maven/MavenMojoProjectParserIT.java
@@ -2,7 +2,6 @@ package org.openrewrite.maven;
 
 import com.soebes.itf.jupiter.extension.*;
 import com.soebes.itf.jupiter.maven.MavenExecutionResult;
-import org.junit.jupiter.api.Disabled;
 
 import static com.soebes.itf.extension.assertj.MavenITAssertions.assertThat;
 
@@ -27,7 +26,6 @@ public class MavenMojoProjectParserIT {
     @MavenOption(value = MavenCLIOptions.SETTINGS, parameter = "settings.xml")
     @MavenProfile("example_profile_id")
     @MavenGoal("${project.groupId}:${project.artifactId}:${project.version}:dryRun")
-    @Disabled("https://github.com/openrewrite/rewrite-maven-plugin/issues/152")
     void maven_mojo_project_parser_tolerates_incomplete_urls_in_settings(MavenExecutionResult result) {
         // Note how we left off this annotation for this test:
         // "@SystemProperty(value = "REPOSITORY_URL", content = "https://maven-eu.nuxeo.org/nexus/content/repositories/public/")"
@@ -38,6 +36,12 @@ public class MavenMojoProjectParserIT {
                 .out()
                 .plain()
                 .allSatisfy(line -> assertThat(line).doesNotContain("Illegal character in path at index 1"));
+
+        assertThat(result)
+                .out()
+                .warn()
+                .hasSize(1)
+                .anySatisfy(line -> assertThat(line).contains("Unable to parse URL ${REPOSITORY_URL} for Maven settings repository id example_repository_id"));
     }
 
 }
