3a1b1e0e3f03024d50dac9eed79b97b52e5ba5a0
Aaron Gershman <aegershman@gmail.com>
2021-12-08 15:03:58 -0600

    Consolidate ITs, syntactic polish, prep for other fixes

--- src/test/java/org/openrewrite/maven/BasicIT.java
+++ src/test/java/org/openrewrite/maven/BasicIT.java
@@ -1,9 +1,6 @@
 package org.openrewrite.maven;
 
-import com.soebes.itf.jupiter.extension.MavenCLIOptions;
-import com.soebes.itf.jupiter.extension.MavenJupiterExtension;
-import com.soebes.itf.jupiter.extension.MavenOption;
-import com.soebes.itf.jupiter.extension.MavenTest;
+import com.soebes.itf.jupiter.extension.*;
 import com.soebes.itf.jupiter.maven.MavenExecutionResult;
 
 import static com.soebes.itf.extension.assertj.MavenITAssertions.assertThat;
@@ -20,6 +17,41 @@ public class BasicIT {
                 .out()
                 .warn()
                 .containsOnly("JAR will be empty - no content was marked for inclusion!");
+    }
+
+    @MavenTest
+    @MavenOption(value = MavenCLIOptions.SETTINGS, parameter = "settings-user.xml")
+    @MavenProfile("example_profile_id")
+    @SystemProperty(value = "REPOSITORY_URL", content = "https://maven-eu.nuxeo.org/nexus/content/repositories/public/")
+    @MavenGoal("${project.groupId}:${project.artifactId}:${project.version}:dryRun")
+    void resolves_settings(MavenExecutionResult result) {
+        assertThat(result)
+                .isSuccessful()
+                .out()
+                .plain()
+                .allSatisfy(line -> assertThat(line).doesNotContain("Illegal character in path at index 1"));
+    }
+
+    @MavenTest
+    @MavenOption(value = MavenCLIOptions.SETTINGS, parameter = "settings-user.xml")
+    @MavenProfile("example_profile_id")
+    @MavenGoal("${project.groupId}:${project.artifactId}:${project.version}:dryRun")
+    void tolerates_incomplete_urls_in_settings(MavenExecutionResult result) {
+        // Note how we left off this annotation for this test:
+        // "@SystemProperty(value = "REPOSITORY_URL", content = "https://maven-eu.nuxeo.org/nexus/content/repositories/public/")"
+        // which leaves the <url>${REPOSITORY_URL}</url> unresolved in the settings.xml.
+        // This tests validates how we handle this situation.
+        assertThat(result)
+                .isSuccessful()
+                .out()
+                .plain()
+                .allSatisfy(line -> assertThat(line).doesNotContain("Illegal character in path at index 1"));
+
+        assertThat(result)
+                .out()
+                .warn()
+                .hasSize(1)
+                .anySatisfy(line -> assertThat(line).contains("Unable to parse URL ${REPOSITORY_URL} for Maven settings repository id example_repository_id"));
     }
 
 }
