1b53a840828a3826902d22867d5594a1d8f3741e
Nick McKinney <mckinneynicholas@gmail.com>
2022-12-08 14:21:10 -0500

    Bugfix: transitive dependencies of recipeArtifactCoordinates jars will now be available during recipe execution (#437) (#445)

---
+++ src/test/resources-its/org/openrewrite/maven/RewriteDryRunIT/no_plugin_in_pom/src/test/java/sample/SampleTest.java
@@ -0,0 +1,13 @@
+package sample;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SampleTest {
+    @Test
+    public void testMethod() {
+        String s = null;
+        assertTrue(s == null);
+    }
+}
