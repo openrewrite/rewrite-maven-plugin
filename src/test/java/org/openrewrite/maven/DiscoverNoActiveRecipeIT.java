b9c00087410f02cc9f77e220da0ac61b981000cf
Tim te Beek <tim@moderne.io>
2023-02-06 23:30:14 +0100

    Restore DiscoverNoActiveRecipeIT after failed local merge

---
+++ src/test/java/org/openrewrite/maven/DiscoverNoActiveRecipeIT.java
@@ -0,0 +1,25 @@
+package org.openrewrite.maven;
+
+import com.soebes.itf.jupiter.extension.MavenCLIOptions;
+import com.soebes.itf.jupiter.extension.MavenGoal;
+import com.soebes.itf.jupiter.extension.MavenJupiterExtension;
+import com.soebes.itf.jupiter.extension.MavenOption;
+import com.soebes.itf.jupiter.extension.MavenTest;
+import com.soebes.itf.jupiter.maven.MavenExecutionResult;
+
+import static com.soebes.itf.extension.assertj.MavenITAssertions.assertThat;
+
+@MavenJupiterExtension
+@MavenOption(MavenCLIOptions.NO_TRANSFER_PROGRESS)
+@MavenGoal("${project.groupId}:${project.artifactId}:${project.version}:discover")
+class DiscoverNoActiveRecipeIT {
+
+    @MavenTest
+    void single_project(MavenExecutionResult result) {
+        assertThat(result)
+                .isSuccessful()
+                .out()
+                .error()
+                .noneSatisfy(line -> assertThat(line).contains("Could not find recipe 'null' among available recipes"));
+    }
+}
