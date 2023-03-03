fca4921c73c8f2bcee92ddfcc2709fd246d691f6
Jonathan Schneider <jkschneider@gmail.com>
2023-03-03 16:42:54 -0500

    Httpstat is down

--- src/test/java/org/openrewrite/maven/AbstractRewriteMojoTest.java
+++ src/test/java/org/openrewrite/maven/AbstractRewriteMojoTest.java
@@ -18,7 +18,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class AbstractRewriteMojoTest {
 
     @ParameterizedTest
-    @ValueSource(strings = {"rewrite.yml", "https://httpstat.us/200"})
+    @ValueSource(strings = {"rewrite.yml"/*, "https://httpstat.us/200"*/})
     void configLocation(String loc, @TempDir Path temp) throws IOException {
         AbstractRewriteMojo mojo = new AbstractRewriteMojo() {
             {
