f1ca40e8059f7fb76915179a9e57c11641551a09
Olga Kundzich <okundzich@gmail.com>
2023-06-17 19:36:54 +0000

    refactor: Common static analysis issues
    Use this link to re-run the recipe: https://app.moderne.io/recipes/org.openrewrite.staticanalysis.CommonStaticAnalysis?organizationId=T3BlblJld3JpdGU%3D
    
    Co-authored-by: Moderne <team@moderne.io>

--- src/test/resources-its/org/openrewrite/maven/RewriteRunIT/multi_module_project/b/src/main/java/sample/EmptyBlockSample.java
+++ src/test/resources-its/org/openrewrite/maven/RewriteRunIT/multi_module_project/b/src/main/java/sample/EmptyBlockSample.java
@@ -6,9 +6,6 @@ import java.util.Random;
 public class EmptyBlockSample implements MyInterface {
     int n = sideEffect();
 
-    static {
-    }
-
     int sideEffect() {
         return new Random().nextInt();
     }
@@ -18,21 +15,20 @@ public class EmptyBlockSample implements MyInterface {
     }
 
     public void lotsOfIfs() {
-        if(sideEffect() == 1) {}
-        if(sideEffect() == sideEffect()) {}
+        sideEffect();
+        sideEffect();
+        sideEffect();
         int n;
-        if((n = sideEffect()) == 1) {}
-        if((n /= sideEffect()) == 1) {}
-        if(new EmptyBlockSample().n == 1) {}
-        if(!boolSideEffect()) {}
-        if(1 == 2) {}
+        n = sideEffect();
+        n /= sideEffect();
+        new EmptyBlockSample();
+        boolSideEffect();
     }
 
     public void emptyTry() {
         try {
             Files.lines(Paths.get("somewhere"));
         } catch (Throwable t) {
-        } finally {
         }
     }
 }
