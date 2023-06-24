ca1ddddc41a7ef8dd6f3841733374b75a0aaed7b
Knut Wannheden <knut@moderne.io>
2023-06-24 22:50:05 +0200

    Attempt to fix broken build

--- src/test/java/org/openrewrite/maven/RewriteDryRunIT.java
+++ src/test/java/org/openrewrite/maven/RewriteDryRunIT.java
@@ -50,7 +50,7 @@ class RewriteDryRunIT {
 
     @MavenTest
     @SystemProperties({
-            @SystemProperty(value = "rewrite.recipeArtifactCoordinates", content = "org.openrewrite.recipe:rewrite-testing-frameworks:2.0.0"),
+            @SystemProperty(value = "rewrite.recipeArtifactCoordinates", content = "org.openrewrite.recipe:rewrite-testing-frameworks:2.0.2"),
             @SystemProperty(value = "activeRecipes", content = "org.openrewrite.java.testing.cleanup.AssertTrueNullToAssertNull")
     })
     void no_plugin_in_pom(MavenExecutionResult result) {
