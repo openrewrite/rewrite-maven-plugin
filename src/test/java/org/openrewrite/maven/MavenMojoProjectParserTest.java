2268f494ec42630fe0cda976c15adf8b5480fa21
Knut Wannheden <knut@moderne.io>
2023-07-07 11:42:24 +0200

    Minor cleanups

--- src/test/java/org/openrewrite/maven/MavenMojoProjectParserTest.java
+++ src/test/java/org/openrewrite/maven/MavenMojoProjectParserTest.java
@@ -1,7 +1,5 @@
 package org.openrewrite.maven;
 
-import it.unimi.dsi.fastutil.ints.IntSets;
-import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.logging.SystemStreamLog;
 import org.apache.maven.project.MavenProject;
 import org.apache.maven.rtinfo.internal.DefaultRuntimeInformation;
@@ -10,22 +8,18 @@ import org.junit.jupiter.api.Test;
 import org.openrewrite.java.marker.JavaVersion;
 import org.openrewrite.marker.Marker;
 
-import java.util.Collection;
-import java.util.Collections;
+import java.nio.file.Paths;
 import java.util.List;
-import java.util.Set;
 
+import static java.util.Collections.emptyList;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
-/**
- * @author Fabian Krüger
- */
 class MavenMojoProjectParserTest {
+
     @Test
     @DisplayName("Given No Java version information exists in Maven Then java.specification.version should be used")
     void givenNoJavaVersionInformationExistsInMavenThenJavaSpecificationVersionShouldBeUsed() {
-        MavenMojoProjectParser sut = new MavenMojoProjectParser(new SystemStreamLog(), null, false, null, new DefaultRuntimeInformation(), false, Collections.EMPTY_LIST, Collections.EMPTY_LIST, -1, null, null, false);
+        MavenMojoProjectParser sut = new MavenMojoProjectParser(new SystemStreamLog(), Paths.get("."), false, null, new DefaultRuntimeInformation(), false, emptyList(), emptyList(), -1, null, null, false);
         List<Marker> markers = sut.generateProvenance(new MavenProject());
         JavaVersion marker = markers.stream().filter(JavaVersion.class::isInstance).map(JavaVersion.class::cast).findFirst().get();
         assertThat(marker.getSourceCompatibility()).isEqualTo(System.getProperty("java.specification.version"));
