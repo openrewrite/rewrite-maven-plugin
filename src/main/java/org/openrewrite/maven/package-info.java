3295c1a5c59221e63bd325c61d3381f02fd3400c
Merlin Bögershausen <merlin.boegershausen@rwth-aachen.de>
2025-01-28 14:26:54 +0100

    polishing with best practices subset

--- src/main/java/org/openrewrite/maven/package-info.java
+++ src/main/java/org/openrewrite/maven/package-info.java
@@ -13,7 +13,5 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-@NullMarked
+@org.jspecify.annotations.NullMarked
 package org.openrewrite.maven;
-
-import org.jspecify.annotations.NullMarked;
