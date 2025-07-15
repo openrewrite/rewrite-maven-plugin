f6f807b114acb27e692f14c05011dc5bbd699563
Steve Elliott <steve@moderne.io>
2025-07-15 14:50:46 -0400

    Adding example of passing JSON as a `rewrite.options` argument value. Adding functionality to swap slash direction in checks based on the local file system so that the tests should be able to be re-enabled for Windows

--- src/test/java/org/openrewrite/maven/RewriteRunIT.java
+++ src/test/java/org/openrewrite/maven/RewriteRunIT.java
@@ -18,142 +18,157 @@ package org.openrewrite.maven;
 import com.soebes.itf.jupiter.extension.*;
 import com.soebes.itf.jupiter.maven.MavenExecutionResult;
 import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.condition.DisabledOnOs;
-import org.junit.jupiter.api.condition.OS;
+
+import java.nio.file.FileSystems;
 
 import static com.soebes.itf.extension.assertj.MavenITAssertions.assertThat;
 
-@DisabledOnOs(OS.WINDOWS)
 @MavenGoal("${project.groupId}:${project.artifactId}:${project.version}:run")
 @MavenJupiterExtension
 @MavenOption(MavenCLIOptions.NO_TRANSFER_PROGRESS)
 @MavenOption(MavenCLIExtra.MUTE_PLUGIN_VALIDATION_WARNING)
 class RewriteRunIT {
+    private static String slashForSystem(String outputLine) {
+        String separator = FileSystems.getDefault().getSeparator();
+        return outputLine.replace("/", separator);
+    }
 
     @MavenTest
     void multi_module_project(MavenExecutionResult result) {
         assertThat(result)
-                .isSuccessful()
-                .out()
-                .warn()
-                .anySatisfy(line -> assertThat(line).contains("org.openrewrite.staticanalysis.SimplifyBooleanExpression"));
+          .isSuccessful()
+          .out()
+          .warn()
+          .anySatisfy(line -> assertThat(line).contains("org.openrewrite.staticanalysis.SimplifyBooleanExpression"));
     }
 
     @MavenGoal("generate-test-sources")
     @MavenTest
     @SystemProperties({
-            @SystemProperty(value = "rewrite.activeRecipes", content = "org.openrewrite.java.search.FindTypes"),
-            @SystemProperty(value = "rewrite.options", content = "fullyQualifiedTypeName=org.junit.jupiter.api.Test")
+      @SystemProperty(value = "rewrite.activeRecipes", content = "org.openrewrite.java.search.FindTypes"),
+      @SystemProperty(value = "rewrite.options", content = "fullyQualifiedTypeName=org.junit.jupiter.api.Test")
     })
     void multi_source_sets_project(MavenExecutionResult result) {
         assertThat(result)
-                .isSuccessful()
-                .out()
-                .warn()
-                .contains(
-                        "Changes have been made to target/maven-it/org/openrewrite/maven/RewriteRunIT/multi_source_sets_project/project/src/integration-test/java/sample/IntegrationTest.java by:",
-                        "Changes have been made to target/maven-it/org/openrewrite/maven/RewriteRunIT/multi_source_sets_project/project/src/test/java/sample/RegularTest.java by:"
-                );
+          .isSuccessful()
+          .out()
+          .warn()
+          .contains(slashForSystem("Changes have been made to target/maven-it/org/openrewrite/maven/RewriteRunIT/multi_source_sets_project/project/src/integration-test/java/sample/IntegrationTest.java by:"))
+          .contains(slashForSystem("Changes have been made to target/maven-it/org/openrewrite/maven/RewriteRunIT/multi_source_sets_project/project/src/test/java/sample/RegularTest.java by:"));
     }
 
     @MavenTest
     void single_project(MavenExecutionResult result) {
         assertThat(result)
-                .isSuccessful()
-                .out()
-                .warn()
-                .anySatisfy(line -> assertThat(line).contains("org.openrewrite.java.format.AutoFormat"));
+          .isSuccessful()
+          .out()
+          .warn()
+          .anySatisfy(line -> assertThat(line).contains("org.openrewrite.java.format.AutoFormat"));
     }
 
     @MavenTest
     void checkstyle_inline_rules(MavenExecutionResult result) {
         assertThat(result)
-                .isSuccessful()
-                .out()
-                .warn()
-                .noneSatisfy(line -> assertThat(line).contains("Unable to parse checkstyle configuration"));
+          .isSuccessful()
+          .out()
+          .warn()
+          .noneSatisfy(line -> assertThat(line).contains("Unable to parse checkstyle configuration"));
     }
 
     @MavenTest
     void recipe_project(MavenExecutionResult result) {
         assertThat(result)
-                .isFailure()
-                .out()
-                .error()
-                .anySatisfy(line -> assertThat(line).contains("/sample/ThrowingRecipe.java", "This recipe throws an exception"));
+          .isFailure()
+          .out()
+          .error()
+          .anySatisfy(line -> assertThat(line).contains(slashForSystem("/sample/ThrowingRecipe.java"), "This recipe throws an exception"));
     }
 
     @MavenTest
     void cloud_suitability_project(MavenExecutionResult result) {
         assertThat(result)
-                .isSuccessful()
-                .out()
-                .warn()
-                .anySatisfy(line -> assertThat(line).contains("some.jks"));
+          .isSuccessful()
+          .out()
+          .warn()
+          .anySatisfy(line -> assertThat(line).contains("some.jks"));
     }
 
     @MavenTest
     @SystemProperties({
-            @SystemProperty(value = "rewrite.activeRecipes", content = "org.openrewrite.maven.RemovePlugin"),
-            @SystemProperty(value = "rewrite.options", content = "groupId=org.openrewrite.maven,artifactId=rewrite-maven-plugin")
+      @SystemProperty(value = "rewrite.activeRecipes", content = "org.openrewrite.maven.RemovePlugin"),
+      @SystemProperty(value = "rewrite.options", content = "groupId=org.openrewrite.maven,artifactId=rewrite-maven-plugin")
     })
     void command_line_options(MavenExecutionResult result) {
         assertThat(result).isSuccessful().out().error().isEmpty();
         assertThat(result).isSuccessful().out().warn()
-                .contains("Changes have been made to target/maven-it/org/openrewrite/maven/RewriteRunIT/command_line_options/project/pom.xml by:")
-                .contains("    org.openrewrite.maven.RemovePlugin: {groupId=org.openrewrite.maven, artifactId=rewrite-maven-plugin}");
+          .contains(slashForSystem("Changes have been made to target/maven-it/org/openrewrite/maven/RewriteRunIT/command_line_options/project/pom.xml by:"))
+          .contains("    org.openrewrite.maven.RemovePlugin: {groupId=org.openrewrite.maven, artifactId=rewrite-maven-plugin}");
         assertThat(result.getMavenProjectResult().getModel().getBuild()).isNull();
+    }
+
+    @SystemProperties({
+      @SystemProperty(value = "rewrite.activeRecipes", content = "org.openrewrite.java.AddCommentToMethod"),
+      @SystemProperty(value = "rewrite.options", content = "comment='{\\\"test\\\":{\\\"some\\\":\\\"yeah\\\"}}',methodPattern=sample.SomeClass doTheThing(..)")
+    })
+    @MavenTest
+    void command_line_options_json(MavenExecutionResult result) {
+        assertThat(result)
+          .isSuccessful()
+          .out()
+          .warn()
+          .contains(slashForSystem("Changes have been made to target/maven-it/org/openrewrite/maven/RewriteRunIT/command_line_options_json/project/src/main/java/sample/SomeClass.java by:"))
+          .contains("    org.openrewrite.java.AddCommentToMethod: {comment='{\"test\":{\"some\":\"yeah\"}}', methodPattern=sample.SomeClass doTheThing(..)}");
     }
 
     @Disabled("We should implement a simpler test to make sure that regular markers don't get added to source files")
     @MavenTest
     void java_upgrade_project(MavenExecutionResult result) {
         assertThat(result)
-                .isSuccessful()
-                .out()
-                .warn()
-                .filteredOn(line -> line.contains("Changes have been made"))
-                .hasSize(1);
+          .isSuccessful()
+          .out()
+          .warn()
+          .filteredOn(line -> line.contains("Changes have been made"))
+          .hasSize(1);
     }
 
     @MavenTest
     void java_compiler_plugin_project(MavenExecutionResult result) {
         assertThat(result)
-                .isSuccessful()
-                .out()
-                .warn()
-                .filteredOn(line -> line.contains("Changes have been made"))
-                .hasSize(1);
+          .isSuccessful()
+          .out()
+          .warn()
+          .filteredOn(line -> line.contains("Changes have been made"))
+          .hasSize(1);
     }
 
     @MavenTest
     void container_masks(MavenExecutionResult result) {
         assertThat(result)
-                .isSuccessful()
-                .out()
-                .warn()
-                .contains(
-                        "Changes have been made to target/maven-it/org/openrewrite/maven/RewriteRunIT/container_masks/project/containerfile.build by:",
-                        "Changes have been made to target/maven-it/org/openrewrite/maven/RewriteRunIT/container_masks/project/Dockerfile by:",
-                        "Changes have been made to target/maven-it/org/openrewrite/maven/RewriteRunIT/container_masks/project/Containerfile by:",
-                        "Changes have been made to target/maven-it/org/openrewrite/maven/RewriteRunIT/container_masks/project/build.dockerfile by:"
-                );
+          .isSuccessful()
+          .out()
+          .warn()
+          .contains(
+            slashForSystem("Changes have been made to target/maven-it/org/openrewrite/maven/RewriteRunIT/container_masks/project/containerfile.build by:"),
+            slashForSystem("Changes have been made to target/maven-it/org/openrewrite/maven/RewriteRunIT/container_masks/project/Dockerfile by:"),
+            slashForSystem("Changes have been made to target/maven-it/org/openrewrite/maven/RewriteRunIT/container_masks/project/Containerfile by:"),
+            slashForSystem("Changes have been made to target/maven-it/org/openrewrite/maven/RewriteRunIT/container_masks/project/build.dockerfile by:")
+          );
     }
 
     @MavenTest
     @SystemProperty(value = "rewrite.additionalPlainTextMasks", content = "**/*.ext,**/.in-root")
     void plaintext_masks(MavenExecutionResult result) {
         assertThat(result)
-                .isSuccessful()
-                .out()
-                .warn()
-                .contains(
-                        "Changes have been made to target/maven-it/org/openrewrite/maven/RewriteRunIT/plaintext_masks/project/src/main/java/sample/in-src.ext by:",
-                        "Changes have been made to target/maven-it/org/openrewrite/maven/RewriteRunIT/plaintext_masks/project/.in-root by:",
-                        "Changes have been made to target/maven-it/org/openrewrite/maven/RewriteRunIT/plaintext_masks/project/from-default-list.py by:",
-                        "Changes have been made to target/maven-it/org/openrewrite/maven/RewriteRunIT/plaintext_masks/project/src/main/java/sample/Dummy.java by:"
-                )
-                .doesNotContain("in-root.ignored");
+          .isSuccessful()
+          .out()
+          .warn()
+          .contains(
+            slashForSystem("Changes have been made to target/maven-it/org/openrewrite/maven/RewriteRunIT/plaintext_masks/project/src/main/java/sample/in-src.ext by:"),
+            slashForSystem("Changes have been made to target/maven-it/org/openrewrite/maven/RewriteRunIT/plaintext_masks/project/.in-root by:"),
+            slashForSystem("Changes have been made to target/maven-it/org/openrewrite/maven/RewriteRunIT/plaintext_masks/project/from-default-list.py by:"),
+            slashForSystem("Changes have been made to target/maven-it/org/openrewrite/maven/RewriteRunIT/plaintext_masks/project/src/main/java/sample/Dummy.java by:")
+          )
+          .doesNotContain("in-root.ignored");
     }
 
 }
---
+++ src/test/resources-its/org/openrewrite/maven/RewriteRunIT/command_line_options_json/src/main/java/sample/SomeClass.java
@@ -0,0 +1,5 @@
+package sample;
+
+public class SomeClass {
+    public void doTheThing() {}
+}
